a0618592a5250494fab0730052cd49f9c9595ead
Allen Burton <aburton@mvgshome.org>
2025-02-18 11:22:03 -0500

    Debugging collapsable search box - getting selected params from URL

--- product_search_scripts/ebay_api_call.php
+++ product_search_scripts/ebay_api_call.php
@@ -111,6 +111,37 @@ function render_results_page($response_decoded, $params) {
     $total_results = isset($response_decoded->total) ? (int)$response_decoded->total : 0;
     echo "<h4>" . ($total_results > 0 ? number_format($total_results) . ' Products Found' : 'No products found. Please refine search.') . "</h4>";
     
+    /////////////////////////////
+
+    $current_query = $_GET;
+    $search_keyword_phrase = isset($_GET['k']) ? $_GET['k'] : '';
+    $condition = isset($_GET['condition']) ? strtoupper($_GET['condition']) : ''; // "NEW" or "USED"
+    $manufacturer = isset($_GET['manufacturer']) ? $_GET['manufacturer'] : '';
+    if ($manufacturer == "Not-Specified" || $manufacturer == "Other" || $manufacturer == "Unbranded") {
+        $manufacturer = '';
+    }
+    $type = isset($_GET['type']) ? $_GET['type'] : '';
+    $additional_filters = isset($_GET['filters']) ? $_GET['filters'] : '';
+
+    // Get special filter names and values from URL parameter list
+    include ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/get_special_filter_pairs_from_url.php');
+    $special_filters = get_special_filter_pairs_from_url();
+    error_log("SPECIAL FILTERS VAR: " . print_r($special_filters, true));
+
+    // Build an associative array
+    $selectedSpecialArr = [];
+    foreach ($special_filters as $pair) {
+        // $pair looks like "fuel=Oil+Fired+Boiler"
+        $parts = explode('=', $pair, 2);
+        if (count($parts) == 2) {
+            $key = urldecode($parts[0]);
+            $val = urldecode($parts[1]);
+            $selectedSpecialArr[$key] = $val;
+        }
+    }
+
+    //////////////////////////////
+
     //Build results filter box for top of page
 	include_once($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/collapsible_filter_box.php');
 	echo displayCollapsibleFilterBox($params['k'], 'results1', $selectedSpecialArr);
