02dce22b3179d47a08f08a49ceb605087e0f3e91
Allen Burton <aburton@mvgshome.org>
2025-02-18 16:23:25 -0500

    Troubleshooting multi-word category problem

--- product_search_scripts/search_filter_scripts.php
+++ product_search_scripts/search_filter_scripts.php
@@ -22,7 +22,6 @@ function get_search_script($unique_id, $product_category, $specialKeys) {
             let btn = document.getElementById('find-products-button-<?php echo $unique_id; ?>');
             if (btn) {
                 btn.addEventListener('click', function () {
-                    console.log("BUTTON CLICKED");
                     const condition = document.querySelector('input[name="condition-<?php echo $unique_id; ?>"]:checked')?.value || '';
 
                     // Manufacturer, Type, Additional Filters
@@ -51,6 +50,11 @@ function get_search_script($unique_id, $product_category, $specialKeys) {
                     const params = new URLSearchParams();
                     if (product_category_<?php echo $unique_id; ?>.indexOf('_') === -1) {
                         params.append('k', product_category_<?php echo $unique_id; ?>);
+                        console.log("PRODUCT CATEGORY (DOES NOT HAVE SLUGS) = " + product_category_<?php echo $unique_id; ?> );
+                    } else {
+                        console.log("PRODUCT CATEGORY (HAS SLUGS) = " + product_category_<?php echo $unique_id; ?> );
+
+                        //params.append('k', product_category_<?php echo $unique_id; ?>);
                     }
                     if (condition) params.append('condition', condition);
                     if (manufacturer) params.append('manufacturer', manufacturer);
