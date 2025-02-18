640e8f3a33b0e8202e1583279d73af06ebca76f9
Allen Burton <aburton@mvgshome.org>
2025-02-18 09:42:02 -0500

    Debugging

--- product_search_scripts/collapsible_filter_box.php
+++ product_search_scripts/collapsible_filter_box.php
@@ -2,8 +2,13 @@
 // collapsible_filter_box.php
 
 // Include necessary helper files
-include_once($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/common_search_functions.php');
-include_once($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/common_search_scripts.php');
+//include_once($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/common_search_functions.php');
+//include_once($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/common_search_scripts.php');
+
+include ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/list_handler.php');
+include ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/general_form_element_functions.php');
+include ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/special_case_form_element_functions.php');
+include ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/search_filter_scripts.php');
 
 $product_category = isset($_GET['k']) ? $_GET['k'] : '';
 
