890b12ab87bbd99d2c8cfdaac38478754af6f346
Allen Burton <aburton@mvgshome.org>
2025-02-18 14:17:01 -0500

    Fixing pagination link display

--- product_search_scripts/ebay_api_call.php
+++ product_search_scripts/ebay_api_call.php
@@ -187,9 +187,9 @@ function render_pagination_links($total_results, $current_page, $results_per_pag
         $query_params['pg'] = $i;
         $query_string = http_build_query($query_params);
         if ($i == $current_page) {
-            $pagination_html .= '<a href="?' . $query_string . '" class="active" style="border:2px;">' . $i . '</a> ';
+            $pagination_html .= '<a href="?' . $query_string . '" class="active" style="border:2px solid;">' . $i . '</a> ';
         } else {
-            $pagination_html .= '<a href="?' . $query_string . '" class="active" style="border:1px;">' . $i . '</a> ';
+            $pagination_html .= '<a href="?' . $query_string . '" class="active" style="border:1px solid;">' . $i . '</a> ';
         }
     }
 
