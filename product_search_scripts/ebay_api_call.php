90b8b2ec66d4ae4aca4403830ca46aecc9b516f5
Allen Burton <aburton@mvgshome.org>
2025-02-19 14:54:11 -0500

    Refactoring ebay API file to be more readable

--- product_search_scripts/ebay_api_call.php
+++ product_search_scripts/ebay_api_call.php
@@ -1,85 +1,15 @@
 <?php
-/** Custom Shortcode added by Burton 8-31-2024  
+/** Custom Shortcode added by Burton 8-31-2024
+ *
+ * This includes the code to take parameters sent via URL and
+ * make a call to eBay's API to get search results then
+ * display the results
  * 
  * v6.0 (02/13/2025): Pulled out of Snippet plugin in WP
  **/
 
 include_once ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/common_search_functions.php');
-
-//////////////////////////////
-// Function Definitions
-/////////////////////////////
-
-
-/**
- * Gets the eBay OAuth token
- * 
- * @return  token
- **/
-function get_ebay_oauth_token() {
-    include ($_SERVER["DOCUMENT_ROOT"] . '/ebay_oauth/getBasicToken.php');
-    return getBasicOauthToken();
-}
-
-/**
- * Builds the search phrase to be used in the API call
- * 
- * @return  string with the conditioned keyword phrase
- **/
-function build_search_keyword_phrase($params) {
-    $phrase = $params['search_keyword_phrase'];
-    if (!empty($params['manufacturer'])) {
-        $phrase .= "+" . $params['manufacturer'];
-    }
-    if (!empty($params['type'])) {
-        $phrase .= "+" . $params['type'];
-    }
-    return str_replace('%2B', '+', rawurlencode($phrase));
-}
-
-/**
- * Constructs the API endpoint
- * 
- * @return  string  API endpoint (url)
- **/
-function construct_api_endpoint($search_keyword_phrase, $params) {
-    $category_id = "12576";
-    $api_endpoint = "https://api.ebay.com/buy/browse/v1/item_summary/search?q=" . $search_keyword_phrase;
-    $api_endpoint .= "&category_ids=" . $category_id;
-    
-    $filters = [];
-    if (!empty($params['condition'])) {
-        $filters[] = 'conditions:{' . $params['condition'] . '}';
-    }
-    if (!empty($params['min_price']) || !empty($params['max_price'])) {
-        $min_price = $params['min_price'] ?: '0';
-        $max_price = $params['max_price'] ?: '999999';
-        $filters[] = 'price:[' . $min_price . '..' . $max_price . '],priceCurrency:USD';
-    }
-    if (!empty($filters)) {
-        $api_endpoint .= "&filter=" . rawurlencode(implode(',', $filters));
-    }
-    $api_endpoint .= "&limit=50&offset=" . (($params['pg'] - 1) * 50) . "&sort=" . (($params['sort_select'] === 'price_asc') ? 'price' : '-price');
-    return $api_endpoint;
-}
-
-/**
- * Gets the data from eBay via the API call
- * 
- * @return  json with the data
- **/
-function fetch_ebay_data($api_endpoint, $auth_token) {
-    $headers = ['Authorization:Bearer ' . $auth_token];
-    $connection = curl_init();
-    curl_setopt($connection, CURLOPT_URL, $api_endpoint);
-    curl_setopt($connection, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($connection, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($connection, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
-    $response = curl_exec($connection);
-    curl_close($connection);
-    return json_decode($response);
-}
+include_once ($_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/ebay_api_call_functions.php');
 
 /**
  * Renders the html to build the page
