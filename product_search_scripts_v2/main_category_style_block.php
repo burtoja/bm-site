4a61c3176a6734009891ee63eff8f2ff33ab7fd7
Allen Burton <aburton@mvgshome.org>
2025-03-25 22:37:07 -0400

    Form is submitting but parameters not encoding right

--- product_search_scripts_v2/main_category_style_block.php
+++ product_search_scripts_v2/main_category_style_block.php
@@ -55,7 +55,7 @@ function render_main_category_listing_style_block() {
             
             #search-button-wrapper {
                 position: sticky;
-                top: 50px;
+                top: 0;
                 padding: 10px;
                 text-align: left;
                 z-index: 1000;
