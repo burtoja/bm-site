cbf6d0c32d0d0e744be41ec90127d8f7e5721be3
Allen Burton <aburton@mvgshome.org>
2025-03-27 15:26:00 -0400

    Debugging script to collect checked filters in each category

--- product_search_scripts_v2/main_category_search_shortcode.php
+++ product_search_scripts_v2/main_category_search_shortcode.php
@@ -53,6 +53,8 @@ function boilersa_categories_shortcode($atts) {
 
     echo '</form>';
 
+    echo '<script src="/product_search_scripts_v2/main_category_search_collect_filters.js"></script>';
+
     return ob_get_clean();
 }
 add_shortcode('boilersa_categories', 'boilersa_categories_shortcode');
