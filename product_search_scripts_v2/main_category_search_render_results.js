f1566a1fb4514cf94f7eaed37c09d26bc6f7151f
Allen Burton <aburton@mvgshome.org>
2025-03-30 17:04:32 -0400

    Style rendered results a bit

--- product_search_scripts_v2/main_category_search_render_results.js
+++ product_search_scripts_v2/main_category_search_render_results.js
@@ -18,10 +18,12 @@ function renderResults(data) {
             <div class="result-card">
                 <a href="${item.itemWebUrl}" target="_blank">
                     <div class="result-image">
-                        <img src="${item.image?.imageUrl || ''}" alt="${item.title}" style="width:100%; height:auto;">
+                        <img src="${item.image?.imageUrl || ''}" alt="${item.title}">
                     </div>
-                    <h5>${item.title}</h5>
-                    <p><strong>$${item.price.value}</strong></p>
+                    <div class="result-details">
+                        <h5 class="title">${item.title}</h5>
+                        <p class="price"><strong>$${item.price.value}</strong></p>
+                    </div>
                 </a>
             </div>
         `;
