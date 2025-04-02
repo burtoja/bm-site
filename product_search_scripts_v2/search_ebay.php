9e71011d6bb86fe641df68b86322d82723aa9f56
Allen Burton <aburton@mvgshome.org>
2025-04-02 13:56:11 -0400

    Debug parameter issue

--- product_search_scripts_v2/search_ebay.php
+++ product_search_scripts_v2/search_ebay.php
@@ -9,7 +9,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts_v2/ebay_api_en
 
 // Debug flag (set to true while testing)
 $debug = false;
-error_log("Error Log Active (search_ebay.php", 3, __DIR__ . "/my_temp_log.txt");
+error_log("Error Log Active (search_ebay.php", 3, $_SERVER["DOCUMENT_ROOT"] ."/my_temp_log.txt");
 
 // Extract query params
 $params = $_GET;
