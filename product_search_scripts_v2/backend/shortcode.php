222aad8bb73b5ba61f8470f6bfbb377eab5cc5bd
Allen Burton <aburton@mvgshome.org>
2025-04-05 13:11:38 -0400

    organizing and improving naming of files

--- product_search_scripts_v2/backend/shortcode.php
+++ product_search_scripts_v2/backend/shortcode.php
@@ -63,7 +63,7 @@ function boilersa_categories_shortcode($atts) {
     //Adding timestamp to this one to force clean cache for testing.
     $ver = time(); // or use a hardcoded version like '1.2'
     echo '<script src="/product_search_scripts_v2/main_category_search_select_category.js?v=' . $ver . '"></script>';
-    echo '<script src="/product_search_scripts_v2/main_category_search_collect_filters.js?v=' . $ver . '"></script>';
+    echo '<script src="/product_search_scripts_v2/collect_filters.js?v=' . $ver . '"></script>';
     echo '<script src="/product_search_scripts_v2/main_category_search_extract_search_params.js?v=' . $ver . '"></script>';
     echo '<script src="/product_search_scripts_v2/build_query.js?v=' . $ver . '"></script>';
     echo '<script src="/product_search_scripts_v2/build_api_endpoint_from_params.js?v=' . $ver . '"></script>';
