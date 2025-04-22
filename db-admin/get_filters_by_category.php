979007b804f6864fa135cd4e987d003b88d06ea9
Allen Burton <aburton@mvgshome.org>
2025-04-22 15:27:58 -0400

    adding db revision for admin -debug

--- db-admin/get_filters_by_category.php
+++ db-admin/get_filters_by_category.php
@@ -1,43 +1,44 @@
 <?php
-require_once 'product_search_scripts_v2/backend/db_connection.php';
+// Temporary mock version to test frontend rendering
 
-$conn = get_db_connection();
+header('Content-Type: application/json');
 
-$categories = [];
-$cat_result = $conn->query("SELECT id, name FROM categories");
-
-while ($cat = $cat_result->fetch_assoc()) {
-    $cat_id = $cat['id'];
-    $filters = [];
-
-    $filter_stmt = $conn->prepare("
-        SELECT f.id, f.name
-        FROM filters f
-        JOIN category_filters cf ON cf.filter_id = f.id
-        WHERE cf.category_id = ?
-    ");
-    $filter_stmt->bind_param("i", $cat_id);
-    $filter_stmt->execute();
-    $filter_result = $filter_stmt->get_result();
-
-    while ($filter = $filter_result->fetch_assoc()) {
-        $filter_id = $filter['id'];
-        $options_stmt = $conn->prepare("SELECT id, value FROM filter_options WHERE filter_id = ?");
-        $options_stmt->bind_param("i", $filter_id);
-        $options_stmt->execute();
-        $options_result = $options_stmt->get_result();
-
-        $filter['options'] = [];
-        while ($opt = $options_result->fetch_assoc()) {
-            $filter['options'][] = $opt;
-        }
-
-        $filters[] = $filter;
-    }
-
-    $cat['filters'] = $filters;
-    $categories[] = $cat;
-}
-
-echo json_encode($categories);
-?>
+echo json_encode([
+    [
+        "id" => 1,
+        "name" => "Heating Systems",
+        "filters" => [
+            [
+                "id" => 101,
+                "name" => "Fuel Type",
+                "options" => [
+                    ["id" => 1001, "value" => "Natural Gas"],
+                    ["id" => 1002, "value" => "Propane"],
+                    ["id" => 1003, "value" => "Electric"]
+                ]
+            ],
+            [
+                "id" => 102,
+                "name" => "Brand",
+                "options" => [
+                    ["id" => 1004, "value" => "Goodman"],
+                    ["id" => 1005, "value" => "Carrier"]
+                ]
+            ]
+        ]
+    ],
+    [
+        "id" => 2,
+        "name" => "Air Conditioners",
+        "filters" => [
+            [
+                "id" => 103,
+                "name" => "Cooling Capacity",
+                "options" => [
+                    ["id" => 1006, "value" => "1 Ton"],
+                    ["id" => 1007, "value" => "2 Ton"]
+                ]
+            ]
+        ]
+    ]
+]);
