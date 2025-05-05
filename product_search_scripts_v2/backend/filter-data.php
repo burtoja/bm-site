579343a9d7322356627e2915485ecbe5de9ce617
Allen Burton <aburton@mvgshome.org>
2025-05-05 19:10:55 -0400

    Alpine integration - file to deliver data json

--- product_search_scripts_v2/backend/filter-data.php
+++ product_search_scripts_v2/backend/filter-data.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/product_search_scripts_v2/backend/db_connection.php');
 global $wpdb;
 
 // Step 1: Get all categories
