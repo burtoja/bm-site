70c818fb64f8a2791a5d53f6aef375bcd4d92ea8
Allen Burton <aburton@mvgshome.org>
2025-06-11 12:17:14 -0400

    Deploying testing scripts

--- product_search_scripts/backend/shortcode.php
+++ product_search_scripts/backend/shortcode.php
@@ -6,7 +6,7 @@ if (!function_exists('get_db_connection')) {
     include $_SERVER["DOCUMENT_ROOT"] . '/product_search_scripts/backend/db_connection.php';
 }
 
-function boilersa_categories_testing_shortcode($atts) {
+function boilersa_search_block_shortcode($atts) {
     $conn = get_db_connection();
 
     ob_start();
@@ -53,4 +53,4 @@ function boilersa_categories_testing_shortcode($atts) {
 
     return ob_get_clean();
 }
-add_shortcode('boilersa_categories-testing', 'boilersa_categories_testing_shortcode');
+add_shortcode('boilersa_search_block', 'boilersa_search_block_shortcode');
